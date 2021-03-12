1bb2f146c9a3426928288c35c7b492cfe620d8d5
nedimpanjeta <panjeta.nedim@gmail.com>
2021-03-12 17:58:43 +0100

    all

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    // Only change code below this line
+      var myName = "Nedim";
+      var myString = "My name is " + myName + " and I am well!";
+      
+
+
+
+    // Onyl change code above this line
+
+    return myString;      
+         
+}
+
+console.log(main());
+module.exports = main
